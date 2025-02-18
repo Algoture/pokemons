97f4e19229b557e5af307545d87174021a3afb17
Umesh Nagare <umeshnagare27@gmail.com>
2025-02-18 22:56:59 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# A ShadCN + Vite + React + JavaScript template
+
+A Vite.js template for building apps with ShadCN UI and JavaScript.
+
+## Usage
+
+```bash
+git clone https://github.com/Algoture/vite-template
+```
+
+## Features
+
+- Radix UI Primitives
+- Tailwind v3.4.17
+- ShadCN UI with JS
+- Icons from [Lucide](https://lucide.dev)
---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <main className="flex items-center justify-center h-screen">
+      <div className="min-h-screen  flex flex-col items-start ml-1 md:ml-3 sm:ml-1 space-y-1 justify-center w-full">
+        <h1 className="text-3xl font-bold leading-tight  tracking-tight">
+          ShadCN + Vite + React + JavaScript
+        </h1>
+        <p className="text-lg text-muted-foreground">
+          A template, fast, and modern UI setup using ShadCN and Vite.
+        </p>
+        <a href="https://github.com/Algoture/vite-template" target="_blank">
+          <Button variant="default">Github</Button>
+        </a>
+      </div>
+    </main>
+  );
+}
+
+export default App;
